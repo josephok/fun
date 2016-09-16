9c0c07caafb51ab01a473798de3c96f13fadc8fa
Joseph <josephok@qq.com>
2016-09-16 17:47:13 +0800

    Add config files

---
+++ cron/spider.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Add this script to crontab:
+# eg.
+# 0 * * * * /home/joe/code/fun/cron/spider.sh
+
+cd /home/joe/code/fun && python3 main.py > logs/spider/stdout.log 2>logs/spider/stderr.log
